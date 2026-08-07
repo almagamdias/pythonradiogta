e10b4299e95950a6ff1cbaf2cf42f9a8d0670e01
almagamdias <dias.almagambetov01@gmail.com>
2026-08-07 21:56:43 +0500

    GTRadio-python v0.1.3b tests

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from radio import RadioEngine
+from pathlib import Path
+
+from radio.util.filesystem import (
+    find_audio,
+    find_logo,
+    list_directories,
+)
 
 
 def main() -> None:
-    engine = RadioEngine()
-    print("GTA Radio Simulator started.")
+    root = Path("test_data/GTA IV")
 
+    print("Directories:")
+
+    for station in list_directories(root):
+        print(f"\n{station.name}")
+
+        print(" Audio:")
+        for file in find_audio(station):
+            print(f"   {file.name}")
+
+        print(" Logo:")
+        for file in find_logo(station):
+            print(f"   {file.name}")
 
 if __name__ == "__main__":
     main()
